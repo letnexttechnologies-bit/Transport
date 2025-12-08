65d0d882041e4fd570b55b3d7bbf42d202fb3065
deepakiam6 <deepaksivakumar126@gmail.com>
2025-12-08 14:29:56 +0530

    first commitffffhhb

--- frontend/src/component/ShipmentDetails.jsx
+++ frontend/src/component/ShipmentDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import "./ShipmentDetails.css";
+import "./shipmentdetails.css";
 
 export default function ShipmentDetails() {
   const { shipmentId } = useParams();
